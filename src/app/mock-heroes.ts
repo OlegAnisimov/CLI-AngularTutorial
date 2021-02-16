805e22d992699cd393da832e8e0a1b9168939181
hohoho <gramafon@yandex.ru>
2021-02-16 17:25:41 +0300

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', weight: 5 },
+  { id: 12, name: 'Narco', weight: 5 },
+  { id: 13, name: 'Bombasto', weight: 5 },
+  { id: 14, name: 'Celeritas', weight: 5 },
+  { id: 15, name: 'Magneta', weight: 5 },
+  { id: 16, name: 'RubberMan', weight: 5 },
+  { id: 17, name: 'Dynama', weight: 5 },
+  { id: 18, name: 'Dr IQ', weight: 5 },
+  { id: 19, name: 'Magma', weight: 5 },
+  { id: 20, name: 'Tornado', weight: 5 }
+];
